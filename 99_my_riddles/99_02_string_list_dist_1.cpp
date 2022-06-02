a49c761461cfd589d3f28912954699544688c3f1
Christoph Kuhmuench <christoph.kuhmuench@email.de>
2022-06-02 16:27:21 -0400

    implementation for 99_2.

--- 99_my_riddles/99_02_string_list_dist_1.cpp
+++ 99_my_riddles/99_02_string_list_dist_1.cpp
@@ -15,6 +15,8 @@
 //  Follow up: function allowing up to "n" edits (harder!)
 //
 
+#include <cassert>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -24,13 +26,43 @@ using namespace std;
 using vec_string_t = vector<string>;
 
 bool is_dist_1(const string &s1, const string &s2) {
+    int l1 = s1.length();
+    int l2 = s2.length();
+    int diff_length = abs(l1 - l2);
 
-    return false;
+    if (diff_length > 1) {
+        return false;
+    }
+    
+    const string *ps1, *ps2;
+    if (s1.length() > s2.length()) {
+        ps1 = &s1; ps2 = &s2;
+    } else {
+        ps1 = &s2; ps2 = &s1;
+    }
+
+    auto it_pair = mismatch(ps1->begin(), ps1->end(), ps2->begin());
+    if (it_pair.first == ps1->end()) {
+        return true;
+    }
+    if (diff_length == 0) {
+        // *it_pair.second =  *it_pair.first;
+        it_pair.first++; it_pair.second++;
+    } else {
+        // diff_len == 1
+        // ps1->erase(it_pair.first)
+        it_pair.first++;
+    }
+    it_pair = mismatch(it_pair.first, ps1->end(), it_pair.second);
+    if (it_pair.first != ps1->end()) {
+        return false;
+    }
+    return true;
 }
 
 bool has_pair_with_dist_1(const vec_string_t &v) {
     for (int i = 0; i < v.size(); i++) {
-        for (int j = i+1; j < v.size(); i++) {
+        for (int j = i+1; j < v.size(); j++) {
             bool rslt = is_dist_1(v[i], v[j]);
             if (rslt) {
                 cout << "found: " << v[i] << " --> " << v[j] << endl;
@@ -43,8 +75,21 @@ bool has_pair_with_dist_1(const vec_string_t &v) {
 
 int main(int argc, char **argv) {
     vec_string_t input_vec {
-        "hello", "hllo", "potatoe", "hexlo"
+        "hello", "potatoe", "hllo", "hexlo"
     };
+
+    bool rslt = is_dist_1("hello", "hallo");
+    cout << rslt << endl;
+    assert(rslt == true);
+    rslt = is_dist_1("hello", "hllo");
+    assert(rslt == true);
+    rslt = is_dist_1("hello", "hllx");
+    assert(rslt == false);
+    rslt = is_dist_1("hello", "hellx");
+    assert(rslt == true);
+
+    rslt = has_pair_with_dist_1(input_vec);
+    assert(rslt == true);
 
     return 0;
 }
